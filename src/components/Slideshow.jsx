1a4ff596fa8f688a381b7a37e47ab1d3921f39c4
rporter03 <porterclayton2003@gmail.com>
2025-11-11 12:39:05 -0500

    house dialog, add house, forms, etc

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,8 +1,7 @@
-import "./../css/Slideshow.css";
-import {useState} from 'react';
+import "../css/Slideshow.css";
+import {useState} from "react";
 
 const Slideshow = () => {
-
     const[slideIndex, setSlideIndex] = useState(0);
 
     const importAll = (resource) => {
@@ -13,28 +12,28 @@ const Slideshow = () => {
         require.context("../images/slideshow", false, /\.(png|jpe?g|svg$|webp)/)
     );
 
-    const slideForward = () => {
-        /*if(slideIndex < images.length - 1) {
+    const slideForward = () =>{
+        /*if(slideIndex < images.length -1){
             setSlideIndex(slideIndex+1);
         } else {
             setSlideIndex(0);
         }*/
-       setSlideIndex(slideIndex < images.length - 1 ? setSlideIndex(slideIndex + 1) : 0);
+       setSlideIndex(slideIndex < images.length-1?slideIndex+1:0);
     };
 
     const slideBackward = () => {
         /*if(slideIndex > 0) {
-            setSlideIndex(slideIndex-1);
+            setSlideIndex(slideIndex -1);
         } else {
-            setSlideIndex(images.length - 1);
+            setSlideIndex(images.length -1);
         }*/
-       setSlideIndex(slideIndex > 0 ? slideIndex - 1 : images.length - 1);
+       setSlideIndex(slideIndex > 0?slideIndex-1:images.length -1);
     };
 
-    return (
+    return(
         <section id="slideshow">
             <img src={images[slideIndex]} />
-            <a className="arrow" onClick={slideForward} id="right-arrow" href="#">&gt;</a>
+            <a className="arrow" onClick = {slideForward} id="right-arrow" href="#">&gt;</a>
             <a className="arrow" onClick={slideBackward} id="left-arrow" href="#">&lt;</a>
         </section>
     );
